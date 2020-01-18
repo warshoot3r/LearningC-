bf13b049a508ef338214084e17e18e7319131081
james <james@JFS1-ADMIN-03>
2020-01-18 13:14:15 +0000

    commit 2

---
+++ C++ program to print number entered by ue/C++ program to print number entered by ue/C++ program to print number entered by ue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int userentered;
+	cout << "Enter a value" << endl;
+ 	cin >> userentered;
+	cout << "Number is ";
+	cout << userentered;
+	return 0;
+}
